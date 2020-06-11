6f2a482465250b6fd1f46a11888480c83a8e4be9
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-06-11 16:49:44 +0200

    [rb-tree] visualizer shows the root

--- TreeVizualizer.py
+++ TreeVizualizer.py
@@ -1,19 +1,44 @@
 import pygame
 import sys
 
+from RedBlackTree import RedBlackTree
 
-def render(root):
+HEIGHT = 600
+WIDTH = 600
+
+WHITE = pygame.Color("white")
+BLUE = pygame.Color("blue")
+CENTER = (WIDTH // 2, HEIGHT // 2)
+
+RADIUS = 20
+
+
+def render(tree: RedBlackTree):
     pygame.init()
+    root = tree.root.value
 
-    size = width, height = 600, 400
+    size = WIDTH, HEIGHT
     black = 0, 0, 0
 
     screen = pygame.display.set_mode(size)
+    font = pygame.font.Font('freesansbold.ttf', 16)
+    text = font.render(str(root), True, BLUE, None)
+    text_rect = text.get_rect()
+    text_rect.center = CENTER
 
     while 1:
+        screen.fill(black)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(black)
+        pygame.draw.circle(screen, WHITE, CENTER, RADIUS)
+        screen.blit(text, text_rect)
+
         pygame.display.flip()
+
+
+if __name__ == '__main__':
+    red_black_tree = RedBlackTree()
+    red_black_tree.put(5)
+    render(red_black_tree)
