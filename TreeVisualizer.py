0c1eda9a32c77b4ca4e6aa4f084c3fb7ed88ae60
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-06-12 10:50:52 +0200

    basics of tree render algorithm

--- TreeVisualizer.py
+++ TreeVisualizer.py
@@ -6,25 +6,47 @@ from RedBlackTree import RedBlackTree
 HEIGHT = 600
 WIDTH = 600
 
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+
 WHITE = pygame.Color("white")
 BLUE = pygame.Color("blue")
-CENTER = (WIDTH // 2, HEIGHT // 2)
+CENTER = (HALF_WIDTH, HALF_HEIGHT)
 
+VERTICAL_SPACING = 100
 RADIUS = 20
+
+
+def draw_tree(tree, screen):
+    draw_sub_tree(tree.root, (HALF_WIDTH, 100), 1, screen)
+
+
+def draw_sub_tree(root, position, height, screen):
+    horizontal_spacing = get_horizontal_spacing(height)
+    x, y = position
+    if root.left is not None:
+        left_position = (x - horizontal_spacing, y + VERTICAL_SPACING)
+        pygame.draw.line(screen, WHITE, position, left_position, 2)
+        draw_sub_tree(root.left, left_position, height + 1, screen)
+    if root.right is not None:
+        right_position = (x + horizontal_spacing, y + VERTICAL_SPACING)
+        pygame.draw.line(screen, WHITE, position, right_position, 2)
+        draw_sub_tree(root.right, right_position, height + 1, screen)
+    draw_circle(screen, root, position)
+
+
+def get_horizontal_spacing(height):
+    split = 2 ** (height + 1)
+    return WIDTH // split
 
 
 def render(tree: RedBlackTree):
     pygame.init()
-    root = tree.root.value
 
     size = WIDTH, HEIGHT
     black = 0, 0, 0
 
     screen = pygame.display.set_mode(size)
-    font = pygame.font.Font('freesansbold.ttf', 16)
-    text = font.render(str(root), True, BLUE, None)
-    text_rect = text.get_rect()
-    text_rect.center = CENTER
 
     while 1:
         screen.fill(black)
@@ -32,13 +54,34 @@ def render(tree: RedBlackTree):
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        pygame.draw.circle(screen, WHITE, CENTER, RADIUS)
-        screen.blit(text, text_rect)
+        draw_tree(tree, screen)
 
         pygame.display.flip()
+
+
+def draw_circle(screen, node, position):
+    pygame.draw.circle(screen, WHITE, position, RADIUS)
+    text, text_rect = text_from_node(node.value, position)
+    screen.blit(text, text_rect)
+
+
+def text_from_node(value, position):
+    font = pygame.font.Font('freesansbold.ttf', 16)
+    text = font.render(str(value), True, BLUE, None)
+    text_rect = text.get_rect()
+    text_rect.center = position
+    return text, text_rect
 
 
 if __name__ == '__main__':
     red_black_tree = RedBlackTree()
     red_black_tree.put(5)
+    red_black_tree.put(2)
+    red_black_tree.put(1)
+    red_black_tree.put(4)
+    red_black_tree.put(7)
+    red_black_tree.put(2)
+    red_black_tree.put(6)
+    red_black_tree.put(9)
+    red_black_tree.put(11)
     render(red_black_tree)
